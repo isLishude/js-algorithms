82ffe623ced8a443c8624aff66aa29db6a48d935
isLishude <lishude@aliyun.com>
2018-02-15 16:00:30 +0800

    feat: add selection sort

---
+++ selectionSort.js
@@ -0,0 +1,17 @@
+const fn = require("./fn")
+
+function selectionSort(arr) {
+    const len = arr.length
+    for (let i = 0; i < len; i++) {
+        let minIndex = i;
+        for (let j = i + 1; j < len; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        fn.swap(arr, i, minIndex)
+    }
+    console.log(arr)
+}
+
+selectionSort(fn.getArray())
