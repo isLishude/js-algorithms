7b54598b0eaff20ef5128e8465c4e92eec9e205c
isLishude <lishude@aliyun.com>
2018-03-02 11:13:49 +0800

    chore: add comments

--- Sort/heap.js
+++ Sort/heap.js
@@ -1,6 +1,7 @@
 const fn = require("../fn")
 
 function shiftDown(arr, pos, len = arr.length) {
+    // max heap shift down
     while (pos * 2 + 1 < len) {
         let j = 2 * pos + 1;
         if (j + 1 < len && arr[j] < arr[j + 1]) j++;
@@ -11,15 +12,16 @@ function shiftDown(arr, pos, len = arr.length) {
 }
 
 function heapSort(arr, len = arr.length) {
-    console.log(arr)
+    // heapify 
     for (let i = Math.floor((len - 2) / 2); i >= 0; i--) {
         shiftDown(arr, i)
     }
-
+    // sorting
     for (let i = len - 1; i > 0; i--) {
         fn.swap(arr, 0, i)
         shiftDown(arr, 0, i)
     }
+    // test
     fn.test(arr)
     return arr
 }
