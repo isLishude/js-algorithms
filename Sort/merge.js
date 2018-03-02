5b23679528e0c85c207c77b4b8c33adf3162c880
isLishude <lishude@aliyun.com>
2018-03-02 15:14:00 +0800

    feat: add merge sort 2

--- Sort/merge.js
+++ Sort/merge.js
@@ -38,4 +38,55 @@ function mergeSort(arr) {
 }
 
 let res = mergeSort(fn.getArray(10, 100))
-console.log(res)
+// console.log(res)
+
+
+function mergeSort2(arr) {
+
+    function merge(arr, l, mid, r) {
+        let len = r - l + 1;
+        let tmp = [];
+        // 复制
+        for (let i = 0; i < len; i++) {
+            tmp[i] = arr[i]
+        }
+
+        let li = l, ri = mid + 1;
+        for (let k = l; k <= r; k++) {
+            if (li > mid) {
+                arr[k] = tmp[ri - l]
+                ri++
+            } else if (ri > r) {
+                arr[k] = tmp[li - l]
+                li++
+            } else if (tmp[li - l] < tmp[ri - l]) {
+                arr[k] = tmp[li - l]
+                li++
+            } else {
+                arr[k] = tmp[ri - l]
+                ri++
+            }
+        }
+    }
+
+    function __mergeSort(arr, li = 0, ri = arr.length - 1) {
+        if (li >= ri) return;
+        let mid = Math.floor((li + ri) / 2);
+        __mergeSort(arr, li, mid);
+        __mergeSort(arr, mid + 1, ri);
+        if (arr[mid] > arr[mid + 1])
+            merge(arr, li, mid, ri)
+    }
+
+    __mergeSort(arr)
+}
+
+let test = fn.getArray(10, 100);
+// 原数组
+console.log(test)
+mergeSort2(test)
+// 排序后
+console.log(test)
+// 测试
+fn.test(test)
+
